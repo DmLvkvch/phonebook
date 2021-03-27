4019347e20c2cd33b91a78ca1c3954f605aae1c9
Дмитрий Левкович <31544742+DmLvkvch@users.noreply.github.com>
2021-03-28 01:17:55 +0300

    Delete phonebook directory

--- phonebook/README.md
+++
@@ -1,2 +0,0 @@
-# phonebook
-Test task DINS
--- phonebook/src/main/java/com/dins/phonebook/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.dins.phonebook.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(Long id) {
-        super("Could not find user ".concat(String.valueOf(id)));
-    }
-}
