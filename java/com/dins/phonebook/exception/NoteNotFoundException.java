1705afe5a4ffd8c07cd9693ee253bb34dbe99392
Дмитрий Левкович <31544742+DmLvkvch@users.noreply.github.com>
2021-03-29 22:50:52 +0300

    Delete java/com/dins/phonebook directory

--- java/com/dins/phonebook/exception/NoteNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.dins.phonebook.exception;
-
-public class NoteNotFoundException extends RuntimeException {
-    public NoteNotFoundException(Long id) {
-        super("Could not find note ".concat(String.valueOf(id)));
-    }
-}
