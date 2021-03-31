db4aee52bbce7fbc10a2de87dc06124f6259bb3b
Дмитрий Левкович <31544742+DmLvkvch@users.noreply.github.com>
2021-03-31 12:37:29 +0300

    Delete src directory

--- src/main/java/com/dins/phonebook/exception/DuplicatePhoneNumber.java
+++
@@ -1,7 +0,0 @@
-package com.dins.phonebook.exception;
-
-public class DuplicatePhoneNumber extends RuntimeException{
-    public DuplicatePhoneNumber(String number){
-        super(String.format("Phone number %s already exists", number));
-    }
-}
