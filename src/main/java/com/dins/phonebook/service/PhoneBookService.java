d9d6f94bdb26310cd5c240366de84e587ae6af8b
Дмитрий Левкович <31544742+DmLvkvch@users.noreply.github.com>
2021-03-31 12:19:31 +0300

    Add files via upload

--- src/main/java/com/dins/phonebook/service/PhoneBookService.java
+++ src/main/java/com/dins/phonebook/service/PhoneBookService.java
@@ -9,9 +9,9 @@ public interface PhoneBookService {
     Optional<PhoneBookNote> getPhoneBookNoteById(Long id);
     Iterable<PhoneBookNote> getUserPhoneBook(Long id);
     EntityModel<PhoneBookNote> getPhoneBookNoteByUserIdAndId(Long id, Long noteId);
-    void deletePhoneBookNote(Long id);
-    void deletePhoneBookNoteByUserIdAndPhoneBookId(Long id, Long phoneBookNoteId);
-    void addPhoneBookNote(Long id, PhoneBookNote phoneBookNote);
-    void updatePhoneBookNoteByUserIdAndId(Long id, Long noteId, String name, String phoneNumber);
+    boolean deletePhoneBookNote(Long id);
+    boolean deletePhoneBookNoteByUserIdAndPhoneBookId(Long id, Long phoneBookNoteId);
+    boolean addPhoneBookNote(Long id, PhoneBookNote phoneBookNote);
+    boolean updatePhoneBookNoteByUserIdAndId(Long id, Long noteId, String name, String phoneNumber);
     Iterable<PhoneBookNote> getPhoneBookNotesByPhoneNumber(String phoneNumber);
 }
